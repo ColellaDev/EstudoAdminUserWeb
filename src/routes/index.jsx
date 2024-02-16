1928f437e13ebb503c7011f8fcaff5b131e4443a
Marcos Colella <colella.dev@gmail.com>
2024-02-16 18:12:19 -0300

    Redirecionando Rotas de acordo com perfil usuário

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
+import { USER_ROLE } from '../utils/roles';
 
 import { AdminRoutes } from './admin.routes';
 import { CustomerRoutes } from './customer.routes';
@@ -10,15 +11,22 @@ import { AuthRoutes } from './auth.routes';
 export function Routes() {
   const { user } = useAuth();
 
-  function AcessRoute() {
-    switch(user.role) {
-      
+  function AccessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes />
+      case USER_ROLE.CUSTOMER:
+        return <CustomerRoutes />
+      case USER_ROLE.SALE:
+        return <SaleRoutes />
+      default:
+        return <CustomerRoutes />
     }
   }
 
   return (
     <BrowserRouter>
-      {user ? <AdminRoutes /> : <AuthRoutes />}
+      {user ? <AccessRoute /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
